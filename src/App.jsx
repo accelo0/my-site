d1823fa41661635a71f77d0454d8c2cc41ed1906
Angelo Pruscino <pruscinoa@gmail.com>
2024-06-04 13:42:36 +0200

    v1.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,69 @@
+import { useState, useEffect } from "react";
 import "./App.css";
-import Body from "./components/Body";
 import Header from "./components/Header";
-import Links from "./components/Links";
-import Welcome from "./components/Welcome";
+import img from "./assets/imgs/pfpImg.jpg";
 
 function App() {
+  const [years, setYears] = useState();
+
+  useEffect(() => {
+    const calcYears =
+      new Date().getFullYear() - new Date("2008-01-27").getFullYear();
+    setYears(calcYears);
+  }, []);
+
   return (
     <>
       <div className="grid place-items-center">
-        <Header />
-        <Links />
-        <Welcome />
-        <Body />
-        <footer className=" mb-6 font-lato font-bold opacity-60">
+        <Header src={img} name="Angelo Pruscino" />
+
+        <div className="relative flex mt-10 mx-8 justify-center w-fit h-fit">
+          <div className="absolute flex items-end inset-0 rounded-t-xl bg-teal-950 blur-3xl opacity-45"></div>
+
+          <div className="relative content-center max-w-[630px] z-10 text-white text-base font-montserrat font-semibold mx-6 my-8">
+            <p>
+              Hello! I’m <span className="font-extrabold">Angelo</span>, a{" "}
+              {years}
+              -year-old guy from <span>Italy</span>, who has a lot of passions,
+              especially coding.
+            </p>
+            <p className="mt-8">
+              I have many interests and I’m trying to cultivate all of them.
+              When I was younger, I thought coding was my only passion, but not
+              long ago, I took a year-long break from coding to focus on my
+              life.
+            </p>
+            <p className="mt-8">
+              During this break, I found out that I’m also interested in{" "}
+              <span>design</span>, particularly web design, which I am currently
+              engaged in. I really love the design process; I love{" "}
+              <span>things that look good and work well</span>, and I appreciate
+              the hard work behind the final product we see.
+            </p>
+            <p className="mt-8">
+              In my everyday use, I have my <span>MacBook</span>, my{" "}
+              <span>iPhone</span>, and my <span>AirPods</span>, especially my
+              AirPods that I can’t live without. I spend a lot of time listening
+              to music.
+            </p>
+            <p className="mt-8">
+              I don’t have extensive experience in coding, but I know{" "}
+              <mark>HTML</mark>, <mark>CSS</mark>, and <mark>JavaScript</mark>{" "}
+              well for vanilla web development. I also like working with{" "}
+              <mark>Node.js</mark> for both frontend and backend, and I’m
+              currently learning some frameworks like <mark>React</mark>. If
+              you’re interested in my projects, feel free to check them out{" "}
+              <a
+                href="https://github.com/accelo0?tab=repositories"
+                className="underline text-green-300 hover:text-green-200"
+              >
+                <i className="fa-solid fa-share-from-square"></i> here
+              </a>
+            </p>
+          </div>
+        </div>
+
+        <footer className=" mb-6 font-lato font-bold opacity-40">
           <p>made with ❤️ and 🎶</p>
         </footer>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,30 @@
-import colors from "../assets/colors.json";
-import img from "../assets/pfpImg.jpg";
-function Header() {
+import Links from "./Links";
+import Welcome from "./Welcome";
+import PropTypes from "prop-types";
+
+function Header({ src, name }) {
   return (
     <>
       <div className="flex justify-center items-center mt-14">
         <div className="grid place-items-center gap-y-4 text-center">
           <img
-            src={img}
-            className={`size-28 rounded-full p-1 border-2 border-spacing-6 border-[${colors["white-green"]}]`}
+            src={src}
+            className={`size-28 rounded-full p-1 border-2 border-spacing-6 border-white-green`}
             alt=""
             draggable={false}
           />
-          <p className="font-montserrat font-extrabold text-3xl">
-            Angelo Pruscino
-          </p>
+          <p className="font-montserrat font-extrabold text-3xl">{name}</p>
         </div>
       </div>
+      <Links />
+      <Welcome />
     </>
   );
 }
+
+Header.propTypes = {
+  src: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
 
 export default Header;
